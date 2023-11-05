69664e2d1b39b0dbd42959d41a367a9a76f1241a
Flex <cma491628@gmail.com>
2023-11-05 10:29:32 +0200

    Методы

--- Task 1/src/Main.java
+++ Task 1/src/Main.java
@@ -2,36 +2,43 @@ import java.util.Scanner;
 
 public class Main {
     static String booksInfo = "";
-
-
     public static void main(String[] args) {
         while (true) {
             String name = inputBookName();
             int pageCount = inputPageCount();
+            double price = inputPrice();
             addBook(name);
             addBook(name, pageCount);
-            //todo использовать новый метод тут
+            addBook(name, pageCount, price);
             printInfo();
         }
     }
 
     public static String inputBookName() {
-        System.out.println("Введите название книги:");
+        System.out.print("Введите название книги: ");
         return new Scanner(System.in).nextLine();
     }
 
     public static int inputPageCount() {
-        System.out.println("Введите кол-во страниц:");
+        System.out.print("Введите кол-во страниц: ");
         return new Scanner(System.in).nextInt();
     }
+    public static double inputPrice() {
+        System.out.print("Ввдите цену: ");
+        return new Scanner(System.in).nextDouble();
+    }
 
-    public static void addBook(String bookName) {
-        addBook(bookName, 0);
+    public static void addBook(String bookName){addBook(bookName, 0);
     }
 
     public static void addBook(String bookName, int pageCount) {
-        booksInfo = booksInfo + bookName + " - " + (pageCount > 0 ? pageCount : "N/A") + " стр.\n";
+        addBook(bookName,  pageCount,  0);
     }
+    public static void addBook(String bookName, int pageCount, double price){
+        booksInfo = booksInfo + bookName + " - " + (pageCount > 0 ? pageCount : "N/A") + " стр."
+                + "Цена - " + (price > 0 ? price + " руб." : "N/A" ) + "\n";
+    }
+
 
     public static void printInfo() {
         System.out.println(booksInfo);
